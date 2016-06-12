3c227351535cc634e92af2f144cfa610679d9533
Idealdo <idealdofelix@gmail.com>
2016-06-11 20:56:01 -0500

    first commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+This is a very simple operating systems tutorial inspired in the following source with some minor changes:
+http://wiki.osdev.org/Bare_Bones
---
+++ ivanOS.sh
@@ -0,0 +1,4 @@
+nasm -felf32 boot.asm -o boot.o
+/usr/local/cross/bin/i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+/usr/local/cross/bin/i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+qemu-system-i386 -kernel myos.bin 
